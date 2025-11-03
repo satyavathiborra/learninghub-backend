3e47899465004c57f44a8dac33f73361bf705db7
Boyapati Thanmai sree <2300030103@kluniversity.in>
2025-11-04 00:20:20 +0530

    fix the enitty model for course repo

--- LearningHub/src/main/java/com/klu/repo/CoursesRepo.java
+++ LearningHub/src/main/java/com/klu/repo/CoursesRepo.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.klu.entity.Courses;
 
-public interface CoursesRepo extends JpaRepository<Courses, Strings>{
+public interface CoursesRepo extends JpaRepository<Courses, String>{
 	
 }
