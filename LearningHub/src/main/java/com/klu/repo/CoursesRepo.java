7ca52deabdbf8b5552ef6681d6307062f6064930
KodaliPavani <2300033843csehte@gmail.com>
2025-11-13 15:35:34 +0530

    Add method to find courses by username

--- LearningHub/src/main/java/com/klu/repo/CoursesRepo.java
+++ LearningHub/src/main/java/com/klu/repo/CoursesRepo.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.klu.entity.Courses;
 
 public interface CoursesRepo extends JpaRepository<Courses, String>{
+	List<Courses> findByUsername(String username);
+
 	
 }
