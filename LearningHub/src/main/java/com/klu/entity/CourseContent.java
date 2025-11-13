edb052b70685780b2b117111d555d08f31744f99
KodaliPavani <2300033843csehte@gmail.com>
2025-11-13 15:38:59 +0530

    Add Jackson annotation for JSON property handling

--- LearningHub/src/main/java/com/klu/entity/CourseContent.java
+++ LearningHub/src/main/java/com/klu/entity/CourseContent.java
@@ -1,5 +1,7 @@
 package com.klu.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
